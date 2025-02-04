a99f3898ce99cd18abbf123d05f849e4bfd77486
aabhiyann <sainjuabhiyan321@gmail.com>
2025-02-04 14:04:04 -0500

    Admin routes and controllers setup. Todo: Album routes and controllers

--- backend/src/lib/db.js
+++ backend/src/lib/db.js
@@ -1,11 +1,11 @@
 import mongoose from "mongoose";
 
 export const connectDB = async () => {
-  try {
-    const conn = await mongoose.connect(process.env.MONGODB_URI);
-    console.log(`Connected to MONGODB ${conn.connection.host}`);
-  } catch (error) {
-    console.log("Error connecting to Mongo DB", error);
-    process.exit(1); // 1 is failure message, 0 is success messsage
-  }
+	try {
+		const conn = await mongoose.connect(process.env.MONGODB_URI);
+		console.log(`Connected to MongoDB ${conn.connection.host}`);
+	} catch (error) {
+		console.log("Failed to connect to MongoDB", error);
+		process.exit(1); // 1 is failure, 0 is success
+	}
 };
