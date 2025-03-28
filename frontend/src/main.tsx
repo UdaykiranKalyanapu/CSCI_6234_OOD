457f2b78c2037ace99254e94ada776dd7f5fb773
aabhiyann <sainjuabhiyan321@gmail.com>
2025-03-28 19:19:00 -0400

    Clerk key setup for login

--- frontend/src/main.tsx
+++ frontend/src/main.tsx
@@ -6,20 +6,21 @@ import { ClerkProvider } from "@clerk/clerk-react";
 import { BrowserRouter } from "react-router-dom";
 import AuthProvider from "./providers/AuthProvider.tsx";
 
+// todo: key for clerk (make it private)
 const PUBLISHABLE_KEY = import.meta.env.VITE_CLERK_PUBLISHABLE_KEY;
 
 if (!PUBLISHABLE_KEY) {
-	throw new Error("Missing Publishable Key");
+  throw new Error("Missing Publishable Key");
 }
 
 createRoot(document.getElementById("root")!).render(
-	<StrictMode>
-		<ClerkProvider publishableKey={PUBLISHABLE_KEY} afterSignOutUrl='/'>
-			<AuthProvider>
-				<BrowserRouter>
-					<App />
-				</BrowserRouter>
-			</AuthProvider>
-		</ClerkProvider>
-	</StrictMode>
+  <StrictMode>
+    <ClerkProvider publishableKey={PUBLISHABLE_KEY} afterSignOutUrl="/">
+      <AuthProvider>
+        <BrowserRouter>
+          <App />
+        </BrowserRouter>
+      </AuthProvider>
+    </ClerkProvider>
+  </StrictMode>
 );
