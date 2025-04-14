d5d788691bdec568ae70e98132859898073197ad
UdaykiranKalyanapu <kalyanapuudaykiran32@gmail.com>
2025-04-14 11:39:22 -0400

    updated cors and axioms for deplyoment

--- backend/src/lib/socket.js
+++ backend/src/lib/socket.js
@@ -4,7 +4,7 @@ import { Message } from "../models/message.model.js";
 export const initializeSocket = (server) => {
 	const io = new Server(server, {
 		cors: {
-			origin: "http://localhost:5173", //frontend
+			origin: process.env.FRONTEND_URL, // pulled from .env
 			credentials: true,
 		},
 	});
