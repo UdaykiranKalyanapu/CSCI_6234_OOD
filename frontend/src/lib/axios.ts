6096e2daa48e32a0b21ed64f8bef4bd5f57c1054
udaykiran Kalyanapu <kalyanapuudaykiran32@gmail.com>
2025-12-07 21:01:33 -0500

    fixed frontend cors

--- frontend/src/lib/axios.ts
+++ frontend/src/lib/axios.ts
@@ -1,8 +1,6 @@
 import axios from "axios";
 
 export const axiosInstance = axios.create({
-  //baseURL: import.meta.env.VITE_API_URL,
-
-  baseURL: "https://csci-6234-ood-1.onrender.com",
+  baseURL: import.meta.env.VITE_API_URL,
   withCredentials: true, // ✅ Important for sending cookies and Clerk auth headers
 });
